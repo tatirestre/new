f73e0112bce0b7bee8c20826f19387b3b923403f
Tatiana Restrepo <trestrepo@lat-trestrepo.local>
2015-11-30 16:55:32 -0500

    added friendly_id

--- Board/app/controllers/jobs_controller.rb
+++ Board/app/controllers/jobs_controller.rb
@@ -64,7 +64,7 @@ class JobsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_job
-      @job = Job.find(params[:id])
+      @job = Job.friendly.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
--- Board/app/models/job.rb
+++ Board/app/models/job.rb
@@ -1,3 +1,5 @@
 class Job < ActiveRecord::Base
 	belongs_to :user
+	extend FriendlyId
+	friendly_id :title, use: [:slugged, :finders]
 end
